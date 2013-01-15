e0f60704f36b8266be327044ded7a1e6fa0c939c
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2013-01-16 03:54:47 +0700

    Use z-sets to sort and filter data

--- test/redis.js
+++ test/redis.js
@@ -6,11 +6,41 @@ var jdb = require('jugglingdb'),
 
 schema.name = 'redis-hq';
 
+var Content = schema.define('Content', {
+    text: jdb.Schema.Text
+});
+
 test(module.exports, schema);
 
 if (test.skip) {
-
     test.skip('hasMany should be cached');
-
 }
+
+test.it('should handle ORDER clause', function (test) {
+    Content.hasMany(schema.models.User, {as: 'author', foreignKey: 'authorId'});
+    Content.create({text: '1'}, function () {
+        Content.create({text: '2'}, function () {
+            Content.all(function (err, contents) {
+                test.equal(contents.length, 2);
+                test.equal(contents[0].text, '2');
+                test.equal(contents[1].text, '1');
+                Content.destroyAll(test.done);
+            });
+        });
+    });
+});
+
+test.it('should handle order clause with direction', function (test) {
+    Content.hasMany(schema.models.User, {as: 'author', foreignKey: 'authorId'});
+    Content.create({text: '1'}, function () {
+        Content.create({text: '2'}, function () {
+            Content.all({reverse: false}, function (err, contents) {
+                test.equal(contents.length, 2);
+                test.equal(contents[0].text, '1');
+                test.equal(contents[1].text, '2');
+                Content.destroyAll(test.done);
+            });
+        });
+    });
+});
 
