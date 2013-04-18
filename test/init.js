c9e5277971f58aed8ed5b3d3c0cef63327dd9bd1
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-04-18 22:50:05 +0400

    Configure db

--- test/init.js
+++ test/init.js
@@ -3,7 +3,7 @@ module.exports = require('should');
 var Schema = require('jugglingdb').Schema;
 
 global.getSchema = function() {
-    var db = new Schema(require('../'), {});
+    var db = new Schema(require('../'), {url: 'localhost', database: 2});
     // db.log = function (a) { console.log(a); };
     return db;
 };
