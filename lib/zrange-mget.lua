f21cd65761980e3f097a61fc48004b38b2993748
Marcus Greenwood <marcusgreenwood@Marcuss-MacBook-Air.local>
2013-09-13 12:09:44 +0100

    Added filter.min and filter.max for ZRANGEBYSCORE on the score of the sorted index

--- lib/zrange-mget.lua
+++ lib/zrange-mget.lua
@@ -1,6 +1,12 @@
-local ids = redis.call('ZRANGE', ARGV[1], tonumber(ARGV[2]), tonumber(ARGV[3]));
+local ids = {};
 local newIds = {};
 local count = 0;
+
+if ARGV[5] and ARGV[6] then
+    ids = redis.call('ZRANGEBYSCORE', ARGV[1], ARGV[5], ARGV[6], 'LIMIT', tonumber(ARGV[2]), tonumber(ARGV[3]));
+else
+    ids = redis.call('ZRANGE', ARGV[1], tonumber(ARGV[2]), tonumber(ARGV[3]));
+end
 
 for i,v in ipairs(ids) do
     newIds[i] = ARGV[4] .. ':' .. v;
--- lib/zrevrange-mget.lua
+++ lib/zrevrange-mget.lua
@@ -1,6 +1,12 @@
-local ids = redis.call('ZREVRANGE', ARGV[1], tonumber(ARGV[2]), tonumber(ARGV[3]));
+local ids = {};
 local newIds = {}; 
 local count = 0;
+
+if ARGV[5] and ARGV[6] then
+    ids = redis.call('ZREVRANGEBYSCORE', ARGV[1], ARGV[5], ARGV[6], 'LIMIT', tonumber(ARGV[2]), tonumber(ARGV[3]));
+else
+    ids = redis.call('ZREVRANGE', ARGV[1], tonumber(ARGV[2]), tonumber(ARGV[3]));
+end
 
 for i,v in ipairs(ids) do
     newIds[i] = ARGV[4] .. ':' .. v;
@@ -10,4 +16,5 @@ end;
 if count == 0 then
     return '';
 end
+
 return redis.call('MGET', unpack(newIds));
