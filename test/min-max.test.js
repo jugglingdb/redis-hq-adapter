b62aed8f8cdfc33943f26eb35676a185c08d04ca
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-09-23 14:19:22 +0100

    Fix filter.min & filter.max, closes #16

---
+++ test/min-max.test.js
@@ -0,0 +1,35 @@
+var should = require('./init.js');
+var db, Hama, log;
+
+describe('min-max', function() {
+
+    before(function(done) {
+        db = getSchema();
+        Hama = db.define('Hama', {
+            name: String,
+            score: {type: Number, index: true}
+        }, {defaultSort: 'score'});
+
+        Hama.destroyAll(function() {
+            Hama.create([
+                {name: 'huda', score: 18},
+                {name: 'mira', score: 71},
+                {name: 'bics', score: 30},
+                {name: 'sica', score: 43}
+            ], function(err, hamas) {
+                done();
+            });
+        });
+    });
+
+    it('should query with range', function(done) {
+        Hama.all({min: 7, max: 20}, function(err, hamas) {
+            should.not.exist(err);
+            should.exist(hamas);
+            hamas.should.have.lengthOf(1);
+            hamas[0].name.should.equal('huda');
+            done();
+        });
+    });
+
+});
